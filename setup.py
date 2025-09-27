90654f2086121a8e28ec2d6d244224142189e108
tarandeep03 <tarandeep.khurana03@gmail.com>
2025-09-27 07:37:38 +0530

    project structure done

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Tarandeep Singh Khurana",
+    author_email="tarandeepkhurana2005@gmail.com",
+    packages=find_packages()
+)
